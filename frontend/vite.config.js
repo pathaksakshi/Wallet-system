90c67b37ff243fd066ef7fe1aed1c9bd6abc5615
SAKSHI RISHIPATHAK <sakshirishipathak@SAKSHIs-Laptop.local>
2025-05-17 17:29:21 +0530

    Docker files added for FE n BE sepearately

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // 👈 This allows external access from Docker
+    port: 5173,      // Optional: Explicitly define the port
     proxy: {
       '/api': {
-        target: process.env.API_URL, // Your backend port
+        target: process.env.API_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
